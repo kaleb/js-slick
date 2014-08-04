98aad6209886189e6c686d2e89167127570b6a41
Kaleb Hornsby <kaleb@horns.by>
2014-08-04 11:22:05 -0400

    Define global

--- amd/index.js
+++ amd/index.js
@@ -1,7 +1,9 @@
 define(function (require, exports, module) {
 /*
 slick
-*/"use strict"
+*/
+"use strict"
+var global = window;
 
 module.exports = "document" in global ? require("./finder") : { parse: require("./parser") }
 
